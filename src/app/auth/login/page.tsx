c9531a79e836d04c73571e21d5d92a1e0832d944
TkymHrt <23.h.takayama.nutfes@gmail.com>
2026-02-23 19:50:21 +0900

    delete: 旧ファイル移動のため削除

--- src/app/auth/login/page.tsx
+++
@@ -1,5 +0,0 @@
-import { LoginView } from "@/features/view/login/LoginView";
-
-export default function LoginPage() {
-  return <LoginView />;
-}
--- src/app/auth/register/page.tsx
+++
@@ -1,5 +0,0 @@
-import { RegisterView } from "@/features/view/register/RegisterView";
-
-export default function RegisterPage() {
-  return <RegisterView />;
-}
